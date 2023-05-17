92847b897b80c0b6d1ef0b11a406d0f509f4d027
15004693200 <15004693200@163.com>
2023-05-17 21:35:29 +0800

    是否识别到鱼10min

--- src/main/java/com/tencent/wxcloudrun/controller/BasicController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/BasicController.java
@@ -114,7 +114,7 @@ public class BasicController {
         String header = httpServletRequest.getHeader("X-WX-OPENID");
         List<UserData> list = userDataService.list(new LambdaQueryWrapper<UserData>()
                 .eq(UserData::getOpenId, header)
-                .between(UserData::getCreateTime, getMinute(-30), new Date())
+                .between(UserData::getCreateTime, getMinute(-10), new Date())
                 .orderByAsc(UserData::getCreateTime));
         List<String> xData = new ArrayList<>();
         List<Double> yData = new ArrayList<>();
